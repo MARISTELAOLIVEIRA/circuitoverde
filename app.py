e29847f032c3e2dfed45f24d7f055982ae2c8794
Maristela <maristela33781197@edu.df.senac.br>
2025-07-14 22:45:17 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -7,5 +7,17 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/teste")
+def teste():
+    return render_template("teste.html")
+
+@app.route("/galeria")
+def galeria():
+    return render_template("galeria.html")
+
+@app.route("/projetos")
+def projetos():
+    return render_template("projetos.html")
+
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
